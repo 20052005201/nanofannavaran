6cc465b5b86a8ed37ba739567b3ef0f66974d207
20052005201 <mynanofannavaran@gmail.com>
2026-07-15 15:35:11 +0330

    Update script.js

--- script.js
+++ script.js
@@ -115,3 +115,30 @@ showSlide(currentSlide);
 });
 
 });
+/*=========================
+     Scroll Animation
+=========================*/
+
+const animatedItems=document.querySelectorAll(".animate");
+
+function reveal(){
+
+animatedItems.forEach(item=>{
+
+const top=item.getBoundingClientRect().top;
+
+const windowHeight=window.innerHeight;
+
+if(top<windowHeight-100){
+
+item.classList.add("show");
+
+}
+
+});
+
+}
+
+window.addEventListener("scroll",reveal);
+
+reveal();
