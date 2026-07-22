38e49975a31ec3f95718c0fa5bedd914b9ec7cc8
20052005201 <mynanofannavaran@gmail.com>
2026-07-22 09:01:03 +0330

    Update script.js

--- script.js
+++ script.js
@@ -166,3 +166,30 @@ document.addEventListener("DOMContentLoaded", function () {
     transform:translateY(0);
     transition:all .7s ease;
 }
+/* ===========================
+   SERVICES ANIMATION
+=========================== */
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const cards = document.querySelectorAll(".service-card");
+
+    const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show-service");
+            }
+
+        });
+
+    }, {
+        threshold: 0.2
+    });
+
+    cards.forEach((card) => {
+        observer.observe(card);
+    });
+
+});
