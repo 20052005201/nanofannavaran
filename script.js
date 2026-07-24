9d8d0e049579ad8da51f696368d53a0224191aa0
20052005201 <mynanofannavaran@gmail.com>
2026-07-24 19:31:13 +0330

    Update script.js

--- script.js
+++ script.js
@@ -361,3 +361,28 @@ backToTop.addEventListener("click", function () {
     });
 
 });
+/* ===========================
+   PROJECT ANIMATION
+=========================== */
+
+const projectCards = document.querySelectorAll(".project-card");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+projectCards.forEach(card=>{
+    observer.observe(card);
+});
